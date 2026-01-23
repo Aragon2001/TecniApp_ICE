64b0a6a16466a5c99ba0ccb92a167ce564ec48ac
Aragon2001 <67432148+Aragon2001@users.noreply.github.com>
2026-01-23 05:58:13 -0600

    Mostrar detalle cliente y estado reparada en edición

--- app/src/main/java/com/Arasoftsolutions/tecniapp_ice/Database/room/AppDatabase.kt
+++ app/src/main/java/com/Arasoftsolutions/tecniapp_ice/Database/room/AppDatabase.kt
@@ -46,7 +46,7 @@ abstract class AppDatabase : RoomDatabase() {
     abstract fun inventarioDao(): InventarioDao
 
     companion object {
-        const val SCHEMA_VERSION = 17
+        const val SCHEMA_VERSION = 18
 
         @Volatile private var INSTANCE: AppDatabase? = null
 
