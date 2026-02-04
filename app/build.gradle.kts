1202a104907894e1294c0e63b585e8754f75e9b5
Aragon2001 <67432148+Aragon2001@users.noreply.github.com>
2026-02-04 08:23:30 -0600

    fix: agregar guava para ListenableFuture

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -135,6 +135,7 @@ dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.8.1")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-play-services:1.8.1")
     implementation("androidx.work:work-runtime-ktx:2.9.1")
+    implementation("com.google.guava:guava:31.1-android")
 
     /* =========================================================
        Networking: Retrofit + Moshi (codegen)
