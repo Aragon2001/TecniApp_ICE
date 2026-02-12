39f441196be68323866375802c44fce5457c7e3b
Jostin Aragon <emanueljab@gamil.com>
2026-02-11 19:17:12 -0600

    CAMBIOS DEL BOTON

---
+++ app/src/main/java/com/Arasoftsolutions/tecniapp_ice/ui/vehiculo/FirebaseVehicleDataSource.kt
@@ -0,0 +1,5 @@
+package com.Arasoftsolutions.tecniapp_ice.ui.vehiculo
+
+private const val VEHICULOS_URL = "https://tecniapp-ice-datosgenerales.firebaseio.com/"
+private const val VEHICULOS_PATH = "datosGenerales/vehiculos"
+
